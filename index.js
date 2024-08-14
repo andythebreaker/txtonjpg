ccb847dcf425f48d30d56024ba0f5d88c3cd43ff
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2024-08-14 18:11:56 +0800

    image up ok

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 import puppeteer from 'puppeteer';
-// Or import puppeteer from 'puppeteer-core';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// Launch the browser and open a new blank page
-const browser = await puppeteer.launch({headless: false});
-const page = await browser.newPage();
+// Get the current file path
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name
+const __dirname = path.dirname(__filename);
 
-// Navigate the page to a URL.
-await page.goto('https://developer.chrome.com/');
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
 
-// Set screen size.
-await page.setViewport({width: 1080, height: 1024});
+  await page.goto('http://127.0.0.1:48489/index.html');
+
+  // Set screen size.
+  await page.setViewport({ width: 1080, height: 1024 });
+  await page.waitForSelector('.ql-editor');
+
+  // Upload the image
+  const fileInput = await page.$('#loadjpg'); 
+  if (fileInput) {
+    const filePath = path.resolve(__dirname, 'a.jpg'); // Path to the image file
+    var file2up=await page.$('#file');
+    await file2up.uploadFile(filePath);
+  } else {
+    console.error('File input element not found!');
+  }
+
+  await page.waitForSelector("#preview"); // Wait for the image upload to complete
+
+  // Call the renderText function if necessary
+  await page.evaluate(() => {
+    renderText("abc");
+  });
+
+
 
 // Type into search box.
-await page.locator('.devsite-search-field').fill('automate beyond recorder');
+//await page.locator('.devsite-search-field').fill('automate beyond recorder');
 
 // Wait and click on first result.
-await page.locator('.devsite-result-item-link').click();
+//await page.locator('.devsite-result-item-link').click();
 
 // Locate the full title with a unique string.
-const textSelector = await page
-  .locator('text/Customize and automate')
-  .waitHandle();
-const fullTitle = await textSelector?.evaluate(el => el.textContent);
+//const textSelector = await page
+//  .locator('text/Customize and automate')
+//  .waitHandle();
+//const fullTitle = await textSelector?.evaluate(el => el.textContent);
 
 // Print the full title.
-console.log('The title of this blog post is "%s".', fullTitle);
+//console.log('The title of this blog post is "%s".', fullTitle);
 
-await browser.close();
+//await browser.close();
